c0d72c0b6eafb8c99e43e3ce9b1055b1566f70d9
rui.ishikawa <rui.ishikawa.business@gmail.com>
2020-08-30 23:19:33 +0900

    [add] ヘッダー追加

---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useStaticQuery, Link, graphql } from 'gatsby'
+import './style.scss'
+
+export default function Header () {
+  const {
+    site: {
+      siteMetadata: { title },
+    },
+  } = useStaticQuery (
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <header id="luxbar" className="luxbar-default">
+      <input type="checkbox" className="luxbar-checkbox" id="luxbar-checkbox"/>
+      <div className="luxbar-menu luxbar-menu-right luxbar-menu-light">
+        <ul className="luxbar-navigation">
+          <li className="luxbar-header">
+            <a href="https://kinnikumegane.com" className="luxbar-brand">{title}</a>
+            <label className="luxbar-hamburger luxbar-hamburger-doublespin" 
+            id="luxbar-hamburger" htmlFor="luxbar-checkbox"> <span></span> </label>
+          </li>
+          <li className="luxbar-item"><Link to="/">Blog</Link></li>
+          <li className="luxbar-item"><Link to="/about">Profile</Link></li>
+          <li className="luxbar-item"><Link to="/portfolio">Portfolio</Link></li>
+          <li className="luxbar-item"><Link to="/privacy">Privacy Policy</Link></li>
+        </ul>
+      </div>
+  </header>
+  )
+}
